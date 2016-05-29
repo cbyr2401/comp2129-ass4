c48663882e75f2b477bbaf0a1d48546c6d3c9db8
Cian Byrne SP3 Sandbox <cian.byrne@coliemore.com.au>
2016-05-29 17:38:08 +1000

    a lot of broken code commented out.  need to fix.

--- pagerank.c
+++ pagerank.c
@@ -19,14 +19,21 @@ void* vector_sumsq_worker(void* argv);
 // matrix operations:
 double* matrix_init(const double value, ssize_t n, ssize_t nthreads);
 void* matrix_init_worker(void* argv);
-void matrix_mul(double* result, const double* matrix, const double* vector, const int n, const int nthreads);
+void matrix_mul(double* result, const double* matrix, const double* vector, const int ncols, const int nrows, const int nthreads);
 void* matrix_mul_worker(void* argv);
 
-#ifdef EBUG
+// reduction operations:
+double* matrix_reduce(double* matrix, ssize_t* map, ssize_t* nrows, ssize_t npages);
+double sum_row(const double* matrix, const int row, const ssize_t width);
+ssize_t compare_sum(const double* sums, const double csum, const int end);
+ssize_t build_matrix(double* result, const double* matrix, const ssize_t* del_rows, const ssize_t npages, const int end_del);
+double* build_vector(const double* vector, const ssize_t* map, const ssize_t npages);
+
+//#ifdef EBUG
 // display:
 void display(const double* matrix, ssize_t npage);
 void display_vector(const double* vector, ssize_t npage);
-#endif
+//#endif
 
 // matrix struct:
 typedef struct {
@@ -90,7 +97,8 @@ void pagerank(node* list, size_t npages, size_t nedges, size_t nthreads, double 
 	// declare matrix and intialise with given value.
 	double* matrix = matrix_init(add_E, npages*npages, nthreads);
 	double* p_previous = matrix_init(div_page, npages, nthreads);
-	double* p_result;
+	double* p_result = NULL;
+	double* p_built = NULL;
 
 	// matrix index temp values:
 	ssize_t i = 0;
@@ -99,6 +107,7 @@ void pagerank(node* list, size_t npages, size_t nedges, size_t nthreads, double 
 	// node temp variables
 	node* current = list;
 	node* inlink = NULL;
+	page* cpage = NULL;
 
 	// variables for the vector norm of each matrix:
 	double norm_result = 0;
@@ -114,20 +123,23 @@ void pagerank(node* list, size_t npages, size_t nedges, size_t nthreads, double 
 		4)  The matrix is initialised as M_hat.
 	*/
 	while(current != NULL){
-		i = current->page->index;
+		cpage = current->page;
+		i = cpage->index;
 
 		// add name to list of keys
-		keys[i] = current->page->name;
+		keys[i] = cpage->name;
+
+		keys[0] = keys[0] + 0;
 
 
-		if(current->page->noutlinks == 0){
+		if(cpage->noutlinks == 0){
 			// go down the column putting in the 1/N, adjusted for M_hat
 			for(j = 0; j < npages; j++){
 				matrix[j * npages + i] += (div_page*dampener);
 			}
 		}
 
-		inlink = current->page->inlinks;
+		inlink = cpage->inlinks;
 		while(inlink != NULL){
 			// calculate 1 / |OUT(j)| for each inlink page, adjusted for M_hat
 			j = inlink->page->index;
@@ -146,6 +158,16 @@ void pagerank(node* list, size_t npages, size_t nedges, size_t nthreads, double 
 		#endif
 	}
 
+	// reduction algorithm:
+	//printf("running reduction\n");
+	//ssize_t* map = (ssize_t*) malloc(sizeof(ssize_t)*npages);  // maps pages --> matrix_row indexes
+	ssize_t nrows = npages;
+
+	//matrix = matrix_reduce(matrix, map, &nrows, npages);
+
+	//printf("finished reduction\n");
+	//printf("nrows: %zu | npages: %zu\n", nrows, npages);
+
 	// We now have the matrix M_hat ready to go...let's start the pagerank iterations.
 	/*
 			Algorithm: Perform iterations (0 to infinity)
@@ -162,29 +184,37 @@ void pagerank(node* list, size_t npages, size_t nedges, size_t nthreads, double 
 		printf("\n");
 	#endif
 
-
+	//int iterations = 0;
 	while(1){
-		p_result = malloc(sizeof(double)*npages);
+		p_result = (double*) malloc(sizeof(double)*nrows);
 
-		matrix_mul(p_result, matrix, p_previous, npages, nthreads);
+		matrix_mul(p_result, matrix, p_previous, npages, nrows, nthreads);
+		p_built = p_result;
+		//printf("build_vector started...");
+		//p_built = build_vector(p_result, map, npages);
+		//printf(" finished \n");
 
 		#ifdef EBUG
-			display_vector(p_result, npages);
+			display_vector(p_built, nrows);
 			printf("\n");
 		#endif
 
-		// calculate the vector norm.
-		norm_result = vector_norm(p_result, p_previous, npages, nthreads);
+		// calculate the vector norm.  TODO: investigate if p_result can be used here.
+		norm_result = vector_norm(p_built, p_previous, npages, nthreads);
 
 		// check for convergence
 		if(norm_result <= EPSILON) break;
 
 		// set up for next iteration...
+		//free(p_result);
 		free(p_previous);
-		p_previous = p_result;
+		p_previous = p_built;
 		p_result = NULL;
+		//printf("iterations: %u\n", iterations++);
 
 	}
+
+	//printf("WE ESCAPED!!!\n");
 
 	#ifdef EBUG
 		display_vector(p_result, npages);
@@ -194,13 +224,128 @@ void pagerank(node* list, size_t npages, size_t nedges, size_t nthreads, double 
 
 	// display results...
 	for(i=0; i < npages; i++){
-		printf("%s %.8lf\n", keys[i], p_result[i]);
+		printf("%s %.8lf\n", keys[i], p_result[i]);  //p_result[map[i]]);
 	}
 
 	// free everything...
 	free(matrix);
 	free(p_result);
+	//free(p_built);
 	free(p_previous);
+	//free(map);
+
+}
+
+
+/**
+ *	Build a larger vector for the next calcuations.
+ */
+double* build_vector(const double* vector, const ssize_t* map, const ssize_t npages){
+	double* result = (double*) malloc(sizeof(double)*npages);
+
+	for(int i = 0; i < npages; i++){
+		result[i] = vector[map[i]];
+	}
+
+	return result;
+}
+
+
+
+/**
+ * 	Reduce the matrix size
+ */
+double* matrix_reduce(double* matrix, ssize_t* map, ssize_t* nrows, ssize_t npages){
+	int next_sum = 0;
+	int next_del = 0;
+	int isSame = -1;
+
+	double sum;
+	double* sums = (double*) malloc(sizeof(double)*npages);
+
+	ssize_t* delete_rows = (ssize_t*) malloc(sizeof(ssize_t)*(npages-1));  // rows to delete in matrix.
+
+
+	for(int row_id = 0; row_id < npages; row_id++){
+		// We already have the maxtrix built, this will form a rectangular matrix, with less rows than the original one.
+		sum = sum_row(matrix, row_id, npages);
+		isSame = compare_sum(sums, sum, next_sum);
+		if(isSame != -1){
+			delete_rows[next_del++] = row_id;
+			map[row_id] = isSame;
+		}else{
+			map[row_id] = row_id-next_del;
+			sums[next_sum++] = sum;
+		}
+	}
+
+	double* result = (double*)malloc(sizeof(double)*npages*npages);
+
+	*nrows = build_matrix(result, matrix, delete_rows, npages, next_del);  // returns number of rows, puts result in first arg.
+
+	free(matrix);
+	free(delete_rows);
+	free(sums);
+
+	return result;
+}
+
+
+
+/**
+ *	Function to sum all the values in a particular row of a given matrix.
+ */
+double sum_row(const double* matrix, const int row, const ssize_t width){
+	double sum = 0.0;
+
+	for(ssize_t i=0; i < width; i++){
+		sum += matrix[row * width + i];
+	}
+
+	return sum;
+}
+
+
+/**
+ *	Function to compare the given sum, to a list of sums.
+ */
+ssize_t compare_sum(const double* sums, const double csum, const int end){
+	for(ssize_t i=0; i < end; i++){
+		if(sums[i] == csum){
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+/**
+ *	Function to build a new matrix, removing the rows that are not wanted (given in an array).
+ */
+ssize_t build_matrix(double* result, const double* matrix, const ssize_t* del_rows, const ssize_t width, const int end_del){
+	if(end_del == 0){
+		// no reduction acheived... this is worst case run time.
+		return width;
+	}
+
+	ssize_t num_rows = width - end_del;
+	int next = 0;
+
+	// columns before...
+	int offset = 0;
+
+	for(ssize_t row = 0; row < num_rows; row++){
+		if(row == del_rows[next]){
+			// when we hit the row that we want to remove, increment the offset to skip that row.
+			offset+=1;
+			next++;
+		}
+		for(int col = 0; col < width; col++){
+			result[(row) * width + (col)] = matrix[(row+offset) * (width) + (col)];
+		}
+	}
+	// return the new, reduced number of rows...
+	return num_rows;
 
 }
 
@@ -437,7 +582,7 @@ void* vector_sub_worker(void* argv){
 /**
  *	Matrix Multiply Thread Controller Process
  */
-void matrix_mul(double* result, const double* matrix, const double* vector, const int n, const int nthreads){
+void matrix_mul(double* result, const double* matrix, const double* vector, const int width, const int height, const int nthreads){
 	// initialise arrays for threading
 	pthread_t thread_ids[nthreads];
 	threadargs args[nthreads];
@@ -451,13 +596,13 @@ void matrix_mul(double* result, const double* matrix, const double* vector, cons
 	
 	// set arguments for worker
 	for(int id=0; id < nthreads; id++){
-		end = id == nthreads - 1 ? n : (id + 1) * (n / nthreads);
+		end = id == nthreads - 1 ? height : (id + 1) * (height / nthreads);
 		args[id] = (threadargs) {
 			.result = result,
 			.matrix = matrix,
 			.vector = vector,
 			.start = start,
-			.width = n,
+			.width = width,
 			.end = end,
 		};
 		start = end;
@@ -503,7 +648,7 @@ void* matrix_mul_worker(void* argv){
 
 
 
-#ifdef EBUG
+//#ifdef EBUG
 /**
  * Displays given matrix.
  */
@@ -528,7 +673,7 @@ void display_vector(const double* vector, ssize_t npage) {
 		printf("\n");
 	}
 }
-#endif
+//#endif
 
 /*
 ######################################
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 ./compile.sh
 #./pagerank 2 < tests/sample.in
-#time ./pagerank 4 < tests/test12.in | diff - tests/test12.out
-./pagerank 2 < tests/test2.in
+time ./pagerank 4 < tests/test12.in | diff - tests/test12.out
+#./pagerank 1 < tests/test12.in
+#./pagerank 2 < tests/test04.in
 #valgrind --show-leak-kinds=all --leak-check=full ./pagerank 4 < tests/sample.in
-head tests/test2.out
+#head tests/test04.out
