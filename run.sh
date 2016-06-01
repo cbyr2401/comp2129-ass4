8b5018e76e5b340d6a95a89b662688d9b3075d3d
Cian Byrne SP3 Sandbox <cian.byrne@coliemore.com.au>
2016-06-01 16:00:42 +1000

    working and tested code with row reduction.

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 ./compile.sh
 #./pagerank 2 < tests/sample.in
 #time ./pagerank 2 < tests/cdog-small.in | diff - tests/cdog-small.out
-time ./pagerank 2 < tests/test12.in | diff - tests/test12.out
+./pagerank 2 < tests/test12.in | diff - tests/test12.out
 #time ./pagerank 2 < tests/test06.in | diff - tests/test06.out
 #./pagerank 4 < tests/cdog-small.in
 #./pagerank 2 < tests/test04.in
 #valgrind --show-leak-kinds=all --leak-check=full ./pagerank 4 < tests/test06.in
-#gprof pagerank
+gprof pagerank
 #head tests/cdog-small.out
