ed7d4f1c3964a2b496a27abc1cb7c8c1fd1d0a37
Cian Byrne SP3 Sandbox <cian.byrne@coliemore.com.au>
2016-06-02 23:40:06 +1000

    added in threading for list sorting... not working. removing next commit

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 ./compile.sh
 #./pagerank 2 < tests/sample.in
 #time ./pagerank 4 < tests/cdog-medium.in | diff - tests/cdog-medium.out
-time ./pagerank 4 < tests/test12.in | diff - tests/test12.out
+#time ./pagerank 4 < tests/test12.in | diff - tests/test12.out
 #time ./pagerank 2 < tests/test06.in | diff - tests/test06.out
 #./pagerank 4 < tests/cdog-medium.in
 #./pagerank 2 < tests/test04.in
-#valgrind --show-leak-kinds=all --leak-check=full ./pagerank 4 < tests/test12.in
+valgrind --show-leak-kinds=all --leak-check=full ./pagerank 4 < tests/test12.in
 #gprof pagerank
 #head tests/cdog-small.out
