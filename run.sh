7bac1449ac07d62d77651eb0e107473a667e25dc
Cian Byrne SP3 Sandbox <cian.byrne@coliemore.com.au>
2016-06-02 22:37:16 +1000

    STOPPED WORK ON COLUMN REDUCTION.  THIS IS MASTER COPY GOOD ROLLBACK WITH ROW REDUCTION.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 ./compile.sh
 #./pagerank 2 < tests/sample.in
-#time ./pagerank 4 < tests/cdog-medium.in | diff - tests/cdog-medium.out
-time ./pagerank 4 < tests/test12.in | diff - tests/test12.out
+time ./pagerank 4 < tests/cdog-medium.in | diff - tests/cdog-medium.out
+#time ./pagerank 4 < tests/test12.in | diff - tests/test12.out
 #time ./pagerank 2 < tests/test06.in | diff - tests/test06.out
 #./pagerank 4 < tests/cdog-medium.in
 #./pagerank 2 < tests/test04.in
